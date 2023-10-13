85fd5295b8f9aa567460c379ecc46ceefb48223c
ankit-at-github <ankit.kumar.pune@gmail.com>
2023-10-13 19:32:22 -0400

    first commit

---
+++ src/main/java/com/learning/apache/kafka/LearningApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.apache.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/apache/kafka/LearningApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.apache.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
